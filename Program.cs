4c1474929664ac558af49eb6d30b8b6303f69b93
andriid <info731408@ukr.net>
2018-05-02 23:56:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace task_exec
+{
+    public class taskexec: System.Threading.Tasks.TaskScheduler
+    {
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public void DoTasks()
+        {
+            string INIfolderPath;
+           
+
+            INIfolderPath = System.IO.Directory.GetCurrentDirectory();
+            INIfolderPath = INIfolderPath + "\\data.ini";
+
+            string[] lines = System.IO.File.ReadAllLines(INIfolderPath);
+
+            // Display the file contents by using a foreach loop.
+            //System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+                var t = Task.Run(() => {
+                    Console.WriteLine("Task thread ID: {0}",
+                Thread.CurrentThread.ManagedThreadId);
+                });
+                t.Wait();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var t = new task_exec.taskexec();
+        t.DoTasks();
+        }
+
+    }
+
